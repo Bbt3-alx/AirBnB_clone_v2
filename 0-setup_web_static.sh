e7ea8cbf7c394eb62d9efdcbd76483a7d11b2de1
Brehyma Traore <brehymatraore50@gmail.com>
2024-07-28 19:03:47 +0000

    A Bash script that sets up your web servers for the deployment of web_static.

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# a Bash script that sets up your web servers for the deployment of web_static. It must
+sudo apt-get update
+sudo apt-get -y install nginx
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+echo "<html>
+	<head>
+	</head>
+	<body>Hello from Nginx !</body>
+</html>" > /data/web_static/releases/test/index.html
+
+if [ -L /data/web_static/current ]; then
+	sudo rm /data/web_static/current
+fi
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/server_name _;/a \ \n\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo service nginx restart
